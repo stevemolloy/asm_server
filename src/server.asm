332c1c9c8ba32863bfda910ef75bedfb2ab172e0
Stephen Molloy <sdmolloy@gmail.com>
2025-06-02 15:56:55 +0200

    Logging

--- src/server.asm
+++ src/server.asm
@@ -6,20 +6,24 @@ global _start
 
 section .text
 _start:
+    WRITE STDOUT, socket_msg, socket_msg_len
     SOCKET AF_INET, SOCK_STREAM, 0
     cmp rax, 0
     jl .exit_fail
     mov [sock], rax
 
-    BIND [sock], addr.sin_family, 16
+    WRITE STDOUT, bind_msg, bind_msg_len
+    BIND [sock], addr.sin_family, addr_len
     cmp rax, 0
     jne .exit_fail
 
+    WRITE STDOUT, listen_msg, listen_msg_len
     LISTEN [sock], BACKLOG
     cmp rax, 0
     jne .exit_fail
 
 .server_loop:
+    WRITE STDOUT, accept_msg, accept_msg_len
     ACCEPT [sock], 0, 0
     cmp rax, 0
     jl .exit_fail
@@ -32,7 +36,7 @@ _start:
     jmp .server_loop
 
 .exit_fail:
-    WRITE STDERR, fail_msg, fail_msg_len
+    WRITE STDERR, err_msg, err_msg_len
     EXIT EXIT_FAILURE
 
 strlen:
@@ -47,16 +51,29 @@ strlen:
     ret
     
 section .data
-    hello db "HTTP/1.1 200 OK", 13, 10, "Content-Type: text/html", 13, 10, 13, 10, "<html><h1>Hello world from ASM</h1></html>"
+    hello db "HTTP/1.1 200 OK", 13, 10, "Content-Type: text/html", 13, 10, 13, 10, "<html><h1>Hello world from ASM</h1><p>Testing</p></html>"
     hello_len equ $ - hello
 
-    fail_msg db "ERROR", 10
-    fail_msg_len equ $ - fail_msg
+    socket_msg db "INFO: Creating socket...", 10
+    socket_msg_len equ $ - socket_msg
+
+    bind_msg db "INFO: Binding to port...", 10
+    bind_msg_len equ $ - bind_msg
+
+    listen_msg db "INFO: Started listening...", 10
+    listen_msg_len equ $ - listen_msg
+
+    accept_msg db "INFO: Awaiting connections...", 10
+    accept_msg_len equ $ - accept_msg
+
+    err_msg db "ERROR", 10
+    err_msg_len equ $ - err_msg
 
     addr.sin_family dw AF_INET
     addr.sin_port dw 36895
     addr.sin_addr dd 0
     addr.sin_zero dq 0
+    addr_len equ $ - addr.sin_family
 
 section .bss
     sock: resd 1
